8a117b676d74b9f21c2e432c95afd57b057bc776
Patrick Lafrance <plafrance@crosemont.qc.ca>
2016-09-27 12:33:52 -0400

    Corrigé inconsistance entre la question et les jeux de test

--- cpp/quiz1/q2.php
+++ cpp/quiz1/q2.php
@@ -2,7 +2,7 @@
 
 require('../quiz.php');
 
-$n=rand(2, 100);
+$n=rand(0,63);
 $rep=pow(2,$n);
 execute("Question 2", "Faites une fonction qui calcule et retourne la <em>n</em>ième puissance de 2 où n est un nombre entier entre 0 et 63.", $rep, '7UAmyogW4n', "
 #include <iostream>
