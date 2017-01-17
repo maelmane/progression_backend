6dd549b3e34c646915ddd212a4260b203eee1c22
Patrick Lafrance <plafrance@crosemont.qc.ca>
2017-01-17 17:26:11 -0500

    amélioré la question cpp/5

--- cpp/quiz1/q5.php
+++ cpp/quiz1/q5.php
@@ -2,7 +2,12 @@
 
 require('../quiz.php');
 
-execute("Question 5", "Faites une fonction qui correspond à la déclaration donnée.", "Bravo les cocos!!!", 'UxbRMDqr0c', "
+$a=rand(0,9);
+$b=rand(0,9);
+$c=rand(0,9);
+$d=rand(0,9);
+
+execute("Question 5", "Faites une fonction qui correspond à la déclaration donnée.", "($a$b$c$d)Bravo les cocos!!!", 'UxbRMDqr0c', "
 #include <iostream>
 #include <cstring>
 
@@ -22,8 +27,8 @@ Retour :
 char *zipper(char *dest, const char *chaine1, const char *chaine2);
 
 int main(){
-    char secret_1[] = \"Baolsccs\";
-    char secret_2[] = \"rv e oo!!!\";
+    char secret_1[] = \"(" . "$b$d" ."Baolsccs\";
+    char secret_2[] = \"$a$c)rv e oo!!!\";
     char *resultat = new char[strlen(secret_1)+strlen(secret_2)];
 
     cout << zipper(resultat, secret_1, secret_2) << endl;
