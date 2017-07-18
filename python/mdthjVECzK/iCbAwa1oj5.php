73551a31abc50fe2ef1399f7b99b606fa7cdb9a6
Patrick Lafrance <plafrance@crosemont.qc.ca>
2017-07-18 18:05:02 -0400

    supprimé les fichiers de questions

--- python/mdthjVECzK/iCbAwa1oj5.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require('../quiz.php');
-
-$phrase='suspendisse eleifend tempor mi non vestibulum sem viverra in mauris placerat facilisis faucibus sed gravida tempus malesuada.';
-
-execute("Question 2", "Faites afficher la phrase donnée en majuscules.",strtoupper($phrase),'', "phrase=\"$phrase\"",'',"","", "8mj36dgi4O.php");
-
-?>
