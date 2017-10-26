54b241022718238dc1cee9881944ad5f5ed5fa37
nickbaz <nicobaz010@live.ca>
2017-10-26 12:17:37 -0400

    Ajout du script de redémarrage

--- app/login.php
+++ app/login.php
@@ -7,7 +7,7 @@ if(isset($_SESSION["user_id"])){
     header('Location: index.php?p=accueil');
 }
 else{
-    
+
     if(isset($_POST["submit"])){
         if(empty($_POST["username"]) || empty($_POST["passwd"])){
             $erreur="Le nom d'utilisateur ou le mot de passe ne peuvent être vides.";
@@ -16,9 +16,9 @@ else{
         {
             $username=$_POST["username"];
             $password=$_POST["passwd"];
-            
+
             #            $ldap = ldap_connect("localhost") or die("Impossible de se connecter au serveur d'authentification. Veuillez contacter l'administrateur");
-            #            ldap_set_option($ldap, LDAP_OPT_PROTOCOL_VERSION, 3);   
+            #            ldap_set_option($ldap, LDAP_OPT_PROTOCOL_VERSION, 3);
             #            $bind = @ldap_bind($ldap, "cn=$username,dc=local", "$password");
             #            if($bind) {
 
@@ -32,7 +32,7 @@ else{
             if(!isset($_GET["p"]))
                 header("Location: index.php?p=accueil");
             else
-                header("Location: index.php?p=$_GET[p]&ID=$_GET[ID]");    
+                header("Location: index.php?p=$_GET[p]&ID=$_GET[ID]");
 
         }
             else {
@@ -57,7 +57,7 @@ else{
              <tr>
              <td>Nom d\'utilisateur</td><td><input name="username" type="text"></td>
              </tr><tr>
-             <td>Mot de passe</td><td><input name="passwd" type="password"></td>
+             <td>Mot de passe jfhdkjfhdksj</td><td><input name="passwd" type="password"></td>
              <tr><td></td><td><a href="inscription.php">s\'inscrire</a></td></tr>
              </tr><tr>
              <td></td><td><input name="submit" type="submit" value="Connexion"></td>
