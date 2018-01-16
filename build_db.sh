b924d9a26d16244ce904bcd24736f6296415aa0f
Patrick Lafrance <plafrance@crosemont.qc.ca>
2018-01-16 00:30:41 -0500

    Changé le script de création de BD pour ajouter la population

--- build_db.sh
+++ build_db.sh
@@ -1,1 +1,1 @@
-cat create_db.sql questions/themes.sql questions/themes/**/serie*.sql | mysql -v -u root > /dev/null
+cat create_db.sql populate_db.sql | mysql -v -u root -h mydb.exemple.com > /dev/null
--- create_db.sql
+++ create_db.sql
@@ -1,7 +1,7 @@
 DROP DATABASE IF EXISTS quiz;
--- DROP USER IF EXISTS quiz@localhost;
+DROP USER IF EXISTS quiz@localhost;
 
--- CREATE USER quiz@localhost IDENTIFIED BY 'password';
+CREATE USER quiz@localhost IDENTIFIED BY 'password';
 CREATE DATABASE quiz;
 
 USE quiz;
