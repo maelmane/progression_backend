a36f3c078c0d7315b4319ba02a62c8c808ce7ec9
Patrick Lafrance <patrick.lafrance@gmail.com>
2018-08-17 01:57:47 -0400

    templatisé serie.php

--- app/vues/serie.php
+++ app/vues/serie.php
@@ -7,8 +7,6 @@ $theme=charger_theme_ou_terminer($serie);
 
 page_header($theme->titre);
 page_content($serie);
-page_footer();
-
 
 function charger_serie_ou_terminer(){
     $serie=new Serie($_GET['ID']);
@@ -18,7 +16,6 @@ function charger_serie_ou_terminer(){
     }
 
     return $serie;
-    
 }
 
 function charger_theme_ou_terminer($serie){
@@ -29,75 +26,45 @@ function charger_theme_ou_terminer($serie){
     }
 
     return $theme;
-    
 }
 
 function page_content($serie){
-    display_serie_header($serie);
-    display_serie_content($serie);
+    $questions=$serie->get_questions();
+    calculer_questions($questions);
+    render_page($serie, $questions);
 }
 
-function display_serie_header($serie){
-    echo "<h3>$serie->titre</h3>
-          <br>
-          $serie->description
-          <br><br>";
-}
- 
-function display_serie_content($serie){
-    $ids=$serie->get_questions();
-    question_header();
-    foreach($ids as $question){
-        question_row($question);
-    }
-    question_footer($serie);
+function calculer_questions($questions){
+    calculer_url($questions);
+    calculer_css($questions, $_SESSION['user_id']);
 }
 
-function question_header(){
-    echo "<div class='code-wrapper'>
-              <table width=100%>";
-}
-
-function question_row($question){
-    $avancement=$question->get_avancement($_SESSION["user_id"]);
-    
-    if($avancement->get_etat() == Question::ETAT_CACHE){
-        display_question_cachee($question);
-    }
-    else{
-        display_question_montree($question);
+function calculer_url($questions){
+    foreach($questions as $question){
+        if($question->type==Question::TYPE_PROG){
+            $question->page="question_prog";
+        }
+        elseif($question->type==Question::TYPE_SYS){
+            $question->page="question_sys";
+        }
+        else{
+            $question->page="erreur";
+        }
     }
 }
 
-function display_question_cachee($question){
-    echo         "<tr>
-                      <td class='questionCachee'>⚪ N°". $question->numero ." : ". $question->titre ."</a>
-                  </tr>";
+function calculer_css($questions, $user_id){
+    foreach($questions as $question){
+        if($question->get_avancement($user_id)->get_etat() == Question::ETAT_REUSSI){
+            $question->css="questionReussie";
+        }
+        else{
+            $question->css="questionNonReussie";
+        }
+    }
 }
 
-function display_question_montree($question){
-    $page=$question->type == Question::TYPE_PROG?"question_prog":"question_sys";
-    echo         "<tr>
-                      <td class='".get_css_class($question)."'><a href='?p=$page&ID=$question->id'>". $question->numero ."</a></td>
-                      <td class='".get_css_class($question)."'><a href='?p=$page&ID=$question->id'>". $question->titre ." </a></td>
-                  </tr>";
-     
+function render_page($serie, $questions){
+    $template=$GLOBALS['mustache']->loadTemplate("serie");
+    echo $template->render(array('serie'=>$serie, 'questions'=>$questions));
 }
-        
-function question_footer($serie){
-    echo         "<tr>
-                      <td> </td>
-                  </tr>
-                      <td colspan=3 align=left><a href=index.php?p=theme&ID=$serie->themeID >↩ Retour à la liste des sujets</a></td>
-                 </tr>
-             </table>
-         </div>";
-}
-
-function get_css_class($question){
-    $avancement=$question->get_avancement($_SESSION["user_id"]);
-    
-    return $avancement->get_etat() != Question::ETAT_REUSSI?"questionNonReussie":"questionReussie";
-}
-
-?>
