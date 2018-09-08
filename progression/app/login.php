377ffacac44fc0679fcdb308ddafbd2952aa65cb
Patrick Lafrance <plafrance@crosemont.qc.ca>
2018-09-08 17:38:43 -0400

    correction de bug

--- progression/app/login.php
+++ progression/app/login.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 require_once(__DIR__.'/config.php');
 require_once(__DIR__.'/modele.php');
 session_start();
