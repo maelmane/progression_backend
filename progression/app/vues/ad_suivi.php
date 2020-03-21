c609f095e62895846d8adf994ed5269d61435c9e
Patrick Lafrance <patrick.lafrance@gmail.com>
2020-03-21 00:03:16 -0400

    Templatisé la page de suivi

--- progression/app/vues/ad_suivi.php
+++ progression/app/vues/ad_suivi.php
@@ -3,71 +3,40 @@
 include('admin.php');
 
 function page_contenu(){
-    afficher_page();
+	$infos=récupérer_paramètres();
+	
+    render_page($infos);
 }
 
-function afficher_page(){
-    if(!isset($_GET['u'])){
-        $users=get_users();
+function récupérer_paramètres(){
+	$infos=array();
+	
+	if(!isset($_GET['u'])){
+        $infos["users"]=get_users();
+	}
+	elseif(!isset($_GET['t'])){
+		$infos["user_id"]=$_GET['u'];
+        $infos["thèmes"]=get_themes();
+		foreach($infos["thèmes"] as $thème){
+			$thème->avancement = $thème->get_pourcentage_avancement($_GET["u"]);
+		}
+	}
+	else{
+		$thème=new Theme($_GET['t']);
+		$infos["thème"]=$thème;
+        $infos["séries"]=$thème->get_series();
+		foreach($infos["séries"] as $série){
+			$série->avancement = $série->get_pourcentage_avancement($_GET["u"]);
+		}
+	}
 
-        echo "<table width=100%>";
-        foreach ($users as $user){
-            echo "<tr>";
-            echo "<td><a href=index.php?p=ad_suivi&u=$user->id>" . $user->username . "</a></td>";
-            echo "</tr>";
-        }
-        echo "</table>";
-    }
-
-    elseif(!isset($_GET['t'])){
-        $themes=get_themes();
-
-        echo "
-           <pre class='code-wrapper'><code>
-              <table width=100%>
-              <th align=left  width=70%>Thème</th><th align=center width=30%>Complété</th>";
-
-
-            foreach ($themes as $theme){
-                echo "
-              <tr>
-                 <td><a href='?p=ad_suivi&u=" . $_GET['u'] . "&t=$theme->id'>$theme->titre</td><td align=center>". strval($theme->get_pourcentage_avancement($_GET['u'])) . "% </a></td>
-              </tr>";
-            }
-
-            echo "</table>";
-    }
-
-    elseif(!isset($_GET['s'])){
-        $theme=new Theme($_GET['t']);
-
-        echo "
-
-        <h3>$theme->titre</h3>
-        <br><br>
-        <pre class='code-wrapper'><code>
-            <table width=100%>
-              <th align=left width=10%>N°</th><th align=left>Sujet</th><th align=center>Complété</th>
-";
-
-        //Séries
-        foreach($theme->get_series() as $serie){
-            echo "
-          <tr>
-          <td>". $serie->numero ."</td><td>". $serie->titre ."</a></td><td align=center>". strval($serie->get_pourcentage_avancement($_GET['u'])) ."%</td>
-          </tr>
-
-         ";
-        }
-
-        echo "
-<td>
-<br>
-</td>
-</tr>
-</table>
-";
-
-    }
+	return $infos;
 }
+
+function render_page($infos){
+	$template=$GLOBALS['mustache']->loadTemplate("ad_suivi");
+	echo $template->render($infos);
+}
+
+
 ?>
