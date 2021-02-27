432bb8423b42d8de0f48c19bb4ea32da169f51fa
Patrick Lafrance <plafrance@crosemont.qc.ca>
2021-02-27 23:15:52 +0000

    Schéma de séquence

--- doc/publish.el
+++ doc/publish.el
@@ -7,60 +7,10 @@
 
 ;;; Code:
 
-(require 'package)
-(package-initialize)
-(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(package-refresh-contents)
-(package-install 'org-plus-contrib)
-(setq org-confirm-babel-evaluate nil)
-
-;; CSS
-(require 'htmlize)
-(setq org-html-htmlize-output-type 'css)
-;; CSS
-
-(require 'org)
-(require 'ox-publish)
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((sql . t)
-   (ditaa . t)
-   (emacs-lisp . t)
-   (python . t)
-   (java . t)
-   (shell . t)
-   (C . t)))
-(setq org-babel-python-command "python3")
-
-;; setting to nil, avoids "Author: x" at the bottom
-(setq user-full-name nil)
-
-(setq org-export-with-section-numbers nil
-      org-export-with-smart-quotes t
-      org-export-with-toc t)
-
-(setq org-html-divs '((preamble "header" "top")
-                      (content "main" "content")
-                      (postamble "footer" "postamble"))
-      org-html-container-element "section"
-      org-html-metadata-timestamp-format "%Y-%m-%d"
-      org-html-checkbox-type 'html
-      org-html-html5-fancy t
-      org-html-validation-link nil
-      org-html-doctype "html5"
-	  org-use-sub-superscripts nil
-	  org-export-with-sub-superscripts nil
-	  org-export-default-language "fr")
-
-(defun org-img-export (path desc format)
-  (cond
-   ((eq format 'html)
-	(format "<img src=\"%s\" alt=\"%s\"/>" path desc))
-   ((eq format 'latex)
-	(format "\\begin{center}\\includegraphics[width=.9\\linewidth]{%s}\\end{center}" path desc))))
-
-(org-add-link-type "img" 'org-img-follow 'org-img-export)
+(defvar site-attachments
+  (regexp-opt '("jpg" "jpeg" "gif" "png" "svg"
+                "ico" "cur" "css" "js" "woff" "html" "pdf"))
+  "File types that are published as static fileTest 11s.")
 
 (setq org-publish-project-alist
       (list
@@ -83,13 +33,14 @@
              :publishing-directory "progression/app/html/doc/contenu/démo/"
              :publishing-function 'org-publish-attachment
              :recursive t)
+       (list "site" :components '("org") '("images"))
        (list "images"
              :base-directory "doc/images"
              :base-extension (regexp-opt '("jpg" "jpeg" "gif" "png" "svg" "ico"))
              :publishing-directory "progression/app/html/doc/images"
              :publishing-function 'org-publish-attachment
              :recursive t)
-       (list "site" :components '("org"))))
+ ))
 
 (provide 'publish)
 ;;; publish.el ends here
