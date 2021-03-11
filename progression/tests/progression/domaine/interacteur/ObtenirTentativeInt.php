7515aa85edcf896b0affb9a18c16a4c7c2c6e193
Radu Octavian Dumitru <raduoctav31@protonmail.com>
2021-03-11 15:32:36 -0500

    Ajout de tests pour interacteur Tentative

---
+++ progression/tests/progression/domaine/interacteur/ObtenirTentativeInt.php
@@ -0,0 +1,57 @@
+<?php
+/*
+  This file is part of Progression.
+
+  Progression is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Progression is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Progression.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+namespace progression\domaine\interacteur;
+
+use progression\domaine\entité\TentativeProg;
+use PHPUnit\Framework\TestCase;
+use \Mockery;
+
+final class ObtenirTentativeIntTests extends TestCase
+{
+	public function test_étant_donné_un_avancement_avec_un_user_id_et_question_id_existant_lorsque_cherché_par_user_id_et_question_id_on_obtient_un_objet_avancementprog_correspondant()
+	{
+		$username = "bob";
+		$question_uri =
+			"https://progression.pages.dti.crosemont.quebec/progression_contenu_demo/les_fonctions_01/appeler_une_fonction_avec_retour";
+		$date = 1614965817;
+
+		$résultat_attendu = new TentativeProg("python", "print()", $date);
+
+		$mockAvancementDAO = Mockery::mock("progression\dao\AvancementProgDAO");
+		$mockAvancementDAO
+			->shouldReceive("get_tentative")
+			->with()
+			->andReturn($résultat_attendu);
+
+		$mockDAOFactory = Mockery::mock("progression\dao\DAOFactory");
+		$mockDAOFactory
+			->allows()
+			->get_avancement_prog_dao()
+			->andReturn($mockAvancementDAO);
+
+		$interacteur = new ObtenirTentativeInt($mockDAOFactory);
+		$résultat_obtenu = $interacteur->get_tentative(
+			$username,
+			$question_uri,
+			$date
+		);
+
+		$this->assertEquals($résultat_attendu, $résultat_obtenu);
+	}
+}
