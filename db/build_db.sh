5bb1673e89f47c170ce493cb70aad1d55f898d98
Patrick Lafrance <patrick.lafrance@gmail.com>
2021-03-16 12:42:44 -0400

    Correction du script.
    Enlevé les affichages
    Enlevé les DROP USER

--- db/build_db.sh
+++ db/build_db.sh
@@ -1,9 +1,7 @@
-mysql --default-character-set=utf8 -v -h$DB_SERVERNAME -uroot -p$DB_PASSWORD <<EOF 
+> /dev/null mysql --default-character-set=utf8 -v -h$DB_SERVERNAME -uroot -p$DB_PASSWORD <<EOF 
 DROP DATABASE IF EXISTS $DB_DBNAME;
-DROP USER IF EXISTS $DB_USERNAME@'localhost';
-DROP USER IF EXISTS $DB_USERNAME@'%';
 
-CREATE USER $DB_USERNAME@'%' IDENTIFIED BY "$DB_PASSWORD";
+CREATE USER IF NOT EXISTS $DB_USERNAME@'%' IDENTIFIED BY "$DB_PASSWORD";
 CREATE DATABASE $DB_DBNAME
 	CHARACTER SET utf8mb4
 	COLLATE utf8mb4_general_ci;
@@ -12,4 +10,4 @@ GRANT ALL PRIVILEGES ON $DB_DBNAME.* TO $DB_USERNAME@'%';
 
 EOF
 
-cat $(dirname $0)/create_db.sql | mysql --default-character-set=utf8 -v -h$DB_SERVERNAME -u$DB_USERNAME -p$DB_PASSWORD $DB_DBNAME
+cat $(dirname $0)/create_db.sql | mysql --default-character-set=utf8 -v -h$DB_SERVERNAME -u$DB_USERNAME -p$DB_PASSWORD $DB_DBNAME > /dev/null
