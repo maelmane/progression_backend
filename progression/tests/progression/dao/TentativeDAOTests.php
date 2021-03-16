f250fb3bea58c04477a41f8048798f5c2c8e8a6e
hbarou <1536306@crosemont.qc.ca>
2021-03-16 17:24:51 -0400

    Test pour le TentativeDAO

---
+++ progression/tests/progression/dao/TentativeDAOTests.php
@@ -0,0 +1,53 @@
+<?php
+/*
+  This file is part of Progression.
+
+  Progression is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Progression is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Progression.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+namespace progression\dao;
+
+use progression\dao\TentativeDAO;
+use PHPUnit\Framework\TestCase;
+
+use function PHPUnit\Framework\assertEquals;
+
+final class TentativeDAOTests extends TestCase
+{
+	public function setUp(): void
+	{
+		EntitéDAO::get_connexion()->begin_transaction();
+	}
+
+	public function tearDown(): void
+	{
+		EntitéDAO::get_connexion()->rollback();
+	}
+
+	public function test_étant_donné_une_TentativeProg_existante_lorsquon_get_tentative_on_obtient_un_object_tentative_de_type_prog()
+	{
+		$tentativeProg = (new TentativeDAO())->get_tentative(
+			"bob",
+			"https://depot.com/roger/questions_prog/fonctions01/appeler_une_fonction",
+			1615696276
+		);
+		$tentativeProg->type = "prog";
+
+		$réponse_attendue = "prog";
+
+		$résponse_observée = $tentativeProg->type;
+
+		assertEquals($réponse_attendue, $résponse_observée);
+	}
+}
