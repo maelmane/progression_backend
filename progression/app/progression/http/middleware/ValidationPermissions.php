f580c3ca3fe6bdba428d361b3785a0b2a1372106
Radu Octavian Dumitru <raduoctav31@protonmail.com>
2021-03-31 17:56:46 -0400

    Enlevé paramètre guard du commentaire

--- progression/app/progression/http/middleware/ValidationPermissions.php
+++ progression/app/progression/http/middleware/ValidationPermissions.php
@@ -30,7 +30,6 @@ class ValidationPermissions
 	 *
 	 * @param  \Illuminate\Http\Request  $request
 	 * @param  \Closure  $next
-	 * @param  string|null  $guard
 	 * @return mixed
 	 */
 	public function handle($request, Closure $next)
