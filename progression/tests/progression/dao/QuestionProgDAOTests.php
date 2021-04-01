f283a19a0419c9a1b53528de0cc9f038759d805f
Radu Octavian Dumitru <raduoctav31@protonmail.com>
2021-04-01 14:51:26 -0400

    Correction format tests

--- progression/tests/progression/dao/QuestionProgDAOTests.php
+++ progression/tests/progression/dao/QuestionProgDAOTests.php
@@ -1,29 +1,26 @@
 <?php
 /*
-  This file is part of Progression.
+	This file is part of Progression.
 
-  Progression is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
+	Progression is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
 
-  Progression is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+	Progression is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-  You should have received a copy of the GNU General Public License
-  along with Progression.  If not, see <https://www.gnu.org/licenses/>.
+	You should have received a copy of the GNU General Public License
+	along with Progression.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 namespace progression\dao;
 
 require_once __DIR__ . "/../../TestCase.php";
 
-use progression\domaine\entité\{Avancement, Exécutable, QuestionProg, Question, Test};
-use progression\dao\AvancementDAO;
-
-use Mockery;
+use progression\domaine\entité\{Exécutable, QuestionProg, Test};
 use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 
 final class QuestionProgDAOTests extends \TestCase
