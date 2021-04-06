520285b7dee1ec050680fed6bb16ad14135f0580
Radu Octavian Dumitru <raduoctav31@protonmail.com>
2021-04-06 16:34:27 -0400

    Ajout de tests middleware

--- progression/tests/progression/http/middleware/ValidationPermissionTests.php
+++ progression/tests/progression/http/middleware/ValidationPermissionTests.php
@@ -50,7 +50,10 @@ final class ValidationPermissionsTests extends TestCase
 			return "OK";
 		});
 
-		$this->assertEquals("OK", $résultat_obtenu);
+		$this->assertEquals(
+			"OK",
+			$résultat_obtenu,
+		);
 	}
 
 	public function test_étant_donné_un_utilisateur_normal_bob_connecté_lorsquon_demande_une_ressource_pour_l_utilisateur_existant_jdoe_on_obtient_erreur_403()
@@ -112,7 +115,7 @@ final class ValidationPermissionsTests extends TestCase
 			json_decode($résultat_obtenu->getContent(), true)
 		);
 	}
-	public function test_étant_donné_un_utilisateur_admin_connecté_lorsquon_demande_une_ressource_pour_l_utilisateur_bob_on_obtient_OK()
+	public function test_étant_donné_un_utilisateur_admin_connecté_lorsquon_demande_une_ressource_pour_l_utilisateur_existant_bob_on_obtient_OK()
 	{
 		$mockRequest = Mockery::mock("Illuminate\Http\Request");
 		$mockRequest
@@ -131,7 +134,97 @@ final class ValidationPermissionsTests extends TestCase
 		$résultat_obtenu = $cobaye->handle($mockRequest, function () {
 			return "OK";
 		});
+		$this->assertEquals(
+			"OK",
+			$résultat_obtenu,
+		);
+	}
 
-		$this->assertEquals("OK", $résultat_obtenu);
+	public function test_étant_donné_un_utilisateur_normal_connecté_lorsquon_demande_une_ressource_pour_null_on_obtient_OK()
+	{
+		$mockRequest = Mockery::mock("Illuminate\Http\Request");
+		$mockRequest
+			->allows()
+			->get("utilisateurConnecté")
+			->andReturn(new User("bob"));
+		$mockRequest
+			->allows()
+			->all()
+			->andReturn([
+				"username" => null,
+			]);
+		$mockRequest->request = $mockRequest;
+
+		$cobaye = new ValidationPermissions();
+		$résultat_obtenu = $cobaye->handle($mockRequest, function () {
+			return "OK";
+		});
+
+		$this->assertEquals(
+			"OK",
+			$résultat_obtenu,
+		);
+	}
+
+	public function test_étant_donné_un_utilisateur_non_connecté_lorsquon_demande_une_ressource_pour_null_on_obtient_erreur_403()
+	{
+		$mockRequest = Mockery::mock("Illuminate\Http\Request");
+		$mockRequest
+			->allows()
+			->get("utilisateurConnecté")
+			->andReturn(null);
+		$mockRequest
+			->allows()
+			->all()
+			->andReturn([
+				"username" => null,
+			]);
+		$mockRequest->request = $mockRequest;
+
+		$cobaye = new ValidationPermissions();
+		$résultat_obtenu = $cobaye->handle($mockRequest, function () {
+			return "OK";
+		});
+
+		$résultat_attendu = [
+			"erreur" => "Accès interdit.",
+		];
+
+		$this->assertEquals(403, $résultat_obtenu->status());
+		$this->assertEquals(
+			$résultat_attendu,
+			json_decode($résultat_obtenu->getContent(), true)
+		);
+	}
+
+	public function test_étant_donné_un_utilisateur_non_connecté_lorsquon_demande_une_ressource_pour_l_utilisateur_existant_bob_on_obtient_erreur_403()
+	{
+		$mockRequest = Mockery::mock("Illuminate\Http\Request");
+		$mockRequest
+			->allows()
+			->get("utilisateurConnecté")
+			->andReturn(null);
+		$mockRequest
+			->allows()
+			->all()
+			->andReturn([
+				"username" => "bob",
+			]);
+		$mockRequest->request = $mockRequest;
+
+		$cobaye = new ValidationPermissions();
+		$résultat_obtenu = $cobaye->handle($mockRequest, function () {
+			return "OK";
+		});
+
+		$résultat_attendu = [
+			"erreur" => "Accès interdit.",
+		];
+
+		$this->assertEquals(403, $résultat_obtenu->status());
+		$this->assertEquals(
+			$résultat_attendu,
+			json_decode($résultat_obtenu->getContent(), true)
+		);
 	}
 }
