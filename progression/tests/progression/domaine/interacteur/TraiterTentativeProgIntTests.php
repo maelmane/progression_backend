ba3fae10e3b480e9c4ba1e598949ce419261cb69
Patrick Lafrance <plafrance@crosemont.qc.ca>
2021-04-07 17:16:45 -0400

    Tests unitaires pour TraiterTentativeProgIntTests avec feedback d'erreur

--- progression/tests/progression/domaine/interacteur/TraiterTentativeProgIntTests.php
+++ progression/tests/progression/domaine/interacteur/TraiterTentativeProgIntTests.php
@@ -1,20 +1,20 @@
 <?php
 /*
-	This file is part of Progression.
+   This file is part of Progression.
 
-	Progression is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
+   Progression is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
-	Progression is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+   Progression is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with Progression.  If not, see <https://www.gnu.org/licenses/>.
-*/
+   You should have received a copy of the GNU General Public License
+   along with Progression.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 namespace progression\domaine\interacteur;
 
@@ -23,36 +23,42 @@ use PHPUnit\Framework\TestCase;
 
 final class TraiterTentativeProgIntTests extends TestCase
 {
-	public function test_étant_donné_une_TentativeProg_correcte_et_une_QuestionProg_lorsquon_les_traite_on_obtient_une_TentativeProg_traitée_et_réussie()
+	public function test_étant_donné_une_TentativeProg_correcte_lorsquon_les_traite_on_obtient_une_TentativeProg_traitée_et_réussie()
 	{
 		$question = new QuestionProg();
 		$question->tests = [
-			new Test("premier test", "1", "ok\n"),
-			new Test("deuxième test", "5", "ok\nok\nok\nok\nok\n"),
+			new Test("premier test", "1", "ok\n", null, "Test 0 passé", "Test 0 échoué"),
+			new Test("deuxième test", "5", "ok\nok\nok\nok\nok\n", null, "Test 1 passé", "Test 1 échoué"),
 		];
 		$question->feedback_pos = "Bravo!";
 		$question->feedback_neg = "Non!";
 
 		$tentative = new TentativeProg("python", "testCode");
-		$tentative->résultats = [
-			new RésultatProg("ok\n", ""),
-			new RésultatProg("ok\nok\nok\nok\nok\n", "")
-		];
+		$tentative->résultats = [new RésultatProg("ok\n", ""), new RésultatProg("ok\nok\nok\nok\nok\n", "")];
+		$résultat_attendu = new TentativeProg("python", "testCode", null, true, 2, "Bravo!", [
+			new RésultatProg("ok\n", "", true, "Test 0 passé"),
+			new RésultatProg("ok\nok\nok\nok\nok\n", "", true, "Test 1 passé"),
+		]);
 
 		$résultat_observé = (new TraiterTentativeProgInt(null))->traiter_résultats($question, $tentative);
 
-		$this->assertEquals(2, $résultat_observé->tests_réussis);
-		$this->assertTrue($résultat_observé->réussi);
-		$this->assertEquals("Bravo!", $résultat_observé->feedback);
+		$this->assertEquals($résultat_attendu, $résultat_observé);
 	}
 
-	public function test_étant_donné_une_TentativeProg_incorrecte_et_une_QuestionProg_lorsquon_les_traite_on_obtient_une_TentativeProg_traitée_et_nonréussie()
+	public function test_étant_donné_une_TentativeProg_incorrecte_lorsquon_les_traite_on_obtient_une_TentativeProg_traitée_et_nonréussie_avec_feedback_positif()
 	{
 		$question = new QuestionProg();
 		$question->tests = [
-			new Test("premier test", "1", "ok\n"),
-			new Test("deuxième test", "5", "ok\nok\nok\nok\nok\n"),
-			new Test("troisième test", "10", "ok\nok\nok\nok\nok\nok\nok\nok\nok\nok\n")
+			new Test("premier test", "1", "ok\n", null, "Test 0 passé", "Test 0 échoué"),
+			new Test("deuxième test", "5", "ok\nok\nok\nok\nok\n", null, "Test 1 passé", "Test 1 échoué"),
+			new Test(
+				"troisième test",
+				"10",
+				"ok\nok\nok\nok\nok\nok\nok\nok\nok\nok\n",
+				null,
+				"Test 2 passé",
+				"Test 2 échoué",
+			),
 		];
 		$question->feedback_pos = "Bravo!";
 		$question->feedback_neg = "As-tu essayé de ne pas faire ça?";
@@ -61,36 +67,84 @@ final class TraiterTentativeProgIntTests extends TestCase
 		$tentative->résultats = [
 			new RésultatProg("ok\n", ""),
 			new RésultatProg("ok\nok\nok\n", ""),
-			new RésultatProg("ok\nok\nok\nok\nok\n", "")
+			new RésultatProg("ok\nok\nok\nok\nok\n", ""),
 		];
+
+		$résultat_attendu = new TentativeProg(
+			"python",
+			"testCode",
+			null,
+			false,
+			1,
+			"As-tu essayé de ne pas faire ça?",
+			[
+				new RésultatProg("ok\n", "", true, "Test 0 passé"),
+				new RésultatProg("ok\nok\nok\n", "", false, "Test 1 échoué"),
+				new RésultatProg("ok\nok\nok\nok\nok\n", "", false, "Test 2 échoué"),
+			],
+		);
 
 		$résultat_observé = (new TraiterTentativeProgInt(null))->traiter_résultats($question, $tentative);
 
-		$this->assertEquals(1, $résultat_observé->tests_réussis);
-		$this->assertFalse($résultat_observé->réussi);
-		$this->assertEquals("As-tu essayé de ne pas faire ça?", $résultat_observé->feedback);
+		$this->assertEquals($résultat_attendu, $résultat_observé);
 	}
 
-	public function test_étant_donné_une_TentativeProg_avec_une_erreur_et_une_QuestionProg_lorsquon_les_traites_on_obtient_une_TentativeProg_traitée_et_nonréussie_mais_sans_feedback()
+	public function test_étant_donné_une_TentativeProg_avec_une_erreur_et_un_feedback_d_erreur_prévu_traites_on_obtient_une_TentativeProg_traitée_et_nonréussie_avec_feedback_d_erreur()
 	{
 		$question = new QuestionProg();
 		$question->tests = [
-			new Test("premier test", "1", "ok\n"),
-			new Test("deuxième test", "5", "ok\nok\nok\nok\nok\n"),
+			new Test("premier test", "1", "ok\n", null, "Test 0 passé", "Test 0 échoué", "Erreur!"),
+			new Test("deuxième test", "5", "ok\nok\nok\nok\nok\n", null, "Test 1 passé", "Test 1 échoué", "Erreur!"),
 		];
 		$question->feedback_pos = "Bravo!";
 		$question->feedback_neg = "As-tu essayé de ne pas faire ça?";
 
 		$tentative = new TentativeProg("python", "testCode");
-		$tentative->résultats = [
-			new RésultatProg("ok\n", ""),
-			new RésultatProg("", "testErreur")
-		];
+		$tentative->résultats = [new RésultatProg("ok\n", ""), new RésultatProg("", "testErreur")];
+
+		$résultat_attendu = new TentativeProg(
+			"python",
+			"testCode",
+			null,
+			false,
+			1,
+			"As-tu essayé de ne pas faire ça?",
+			[new RésultatProg("ok\n", "", true, "Test 0 passé"), new RésultatProg("", "testErreur", false, "Erreur!")],
+		);
 
 		$résultat_observé = (new TraiterTentativeProgInt(null))->traiter_résultats($question, $tentative);
 
-		$this->assertEquals(1, $résultat_observé->tests_réussis);
-		$this->assertFalse($résultat_observé->réussi);
-		$this->assertNull($résultat_observé->feedback);
+		$this->assertEquals($résultat_attendu, $résultat_observé);
+	}
+
+	public function test_étant_donné_une_TentativeProg_avec_une_erreur_sans_feedback_d_erreur_prévu_traites_on_obtient_une_TentativeProg_traitée_et_nonréussie_avec_feedback_négatif()
+	{
+		$question = new QuestionProg();
+		$question->tests = [
+			new Test("premier test", "1", "ok\n", null, "Test 0 passé", "Test 0 échoué"),
+			new Test("deuxième test", "5", "ok\nok\nok\nok\nok\n", null, "Test 1 passé", "Test 1 échoué"),
+		];
+		$question->feedback_pos = "Bravo!";
+		$question->feedback_neg = "As-tu essayé de ne pas faire ça?";
+
+		$tentative = new TentativeProg("python", "testCode");
+		$tentative->résultats = [new RésultatProg("ok\n", ""), new RésultatProg("", "testErreur")];
+
+		$résultat_attendu = new TentativeProg(
+			"python",
+			"testCode",
+			null,
+			false,
+			1,
+			"As-tu essayé de ne pas faire ça?",
+			[
+				new RésultatProg("ok\n", "", true, "Test 0 passé"),
+				new RésultatProg("", "testErreur", false, "Test 1 échoué"),
+			],
+		);
+
+		$résultat_observé = (new TraiterTentativeProgInt(null))->traiter_résultats($question, $tentative);
+
+		$this->assertEquals($résultat_attendu, $résultat_observé);
 	}
 }
