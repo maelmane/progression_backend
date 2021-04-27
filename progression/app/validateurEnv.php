f0fc9cfa0b87d7801518f93fcca28fec7453e3a2
Hamed Tijane Mkpoumie Monfouayi <hmkpoumiemonfouayi@dept-info.crosemont.quebec>
2021-04-27 10:46:37 -0400

    Mise a jour de la validation des variables d'environnement

--- progression/app/validateurEnv.php
+++ progression/app/validateurEnv.php
@@ -4,27 +4,28 @@ require_once "/var/www/progression/vendor/" . 'autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable("/var/www/progression/app/");
 $dotenv->load();
 
-$dotenv->required('APP_URL')->allowedValues(['http://localhost/', 'http://172.20.0.3/']);
-$dotenv->required('APP_NAME')->allowedValues(['Progression', 'Lumen']);
+$dotenv->required('APP_URL')->allowedRegexValues('(.*)');
+$dotenv->required('APP_NAME')->allowedValues(['Progression']);
 $dotenv->required('APP_ENV')->allowedValues(['local']);
-$dotenv->required('APP_DEBUG')->isBoolean();
 $dotenv->required('APP_TIMEZONE')->allowedValues(['UTC']);
 
-$dotenv->required('DB_SERVERNAME')->allowedValues(['localhost', '172.20.0.2']);
-$dotenv->required('DB_DBNAME')->allowedValues(['quiz']);
-$dotenv->required('DB_USERNAME')->allowedValues(['root']);
-$dotenv->required('DB_PASSWORD')->allowedValues(['password']);
+$dotenv->required('JWT_SECRET')->allowedRegexValues('(.*)');
+$dotenv->required('DB_SERVERNAME')->allowedRegexValues('(.*)');
+$dotenv->required('DB_DBNAME')->allowedRegexValues('(.*)');
+$dotenv->required('DB_USERNAME')->allowedRegexValues('([A-z]+)');
+$dotenv->required('DB_PASSWORD')->allowedRegexValues('(.*)');
 
-$dotenv->required('AUTH_TYPE')->isBoolean();
-$dotenv->required('HTTP_ORIGIN')->notEmpty();
+$dotenv->required('AUTH_TYPE')->allowedRegexValues('(no|local|ldap)');
+$dotenv->required('HTTP_ORIGIN')->allowedRegexValues('(.*)');
 
 $dotenv->required('LIMITE_YML')->isInteger();
 $dotenv->required('JWT_TTL')->isInteger();
 
-$dotenv->required('COMPILEBOX_URL')->allowedValues(['http://localhost:12380/', 'http://progression.dti.crosemont.quebec:12380/compile']);
+$dotenv->required('COMPILEBOX_URL')->allowedRegexValues('(.*)');
 
+$dotenv->required('APP_DEBUG')->isBoolean();
 $dotenv->required('LOG_CHANNEL')->allowedValues(['stack']);
 $dotenv->required('CACHE_DRIVER')->allowedValues(['file']);
 $dotenv->required('QUEUE_CONNECTION')->allowedValues(['sync']);
 
-$dotenv->required(['JWT_SECRET', 'LOG_SLACK_WEBHOOK_URL', 'APP_KEY']);
+$dotenv->required(['LOG_SLACK_WEBHOOK_URL', 'APP_KEY']);
