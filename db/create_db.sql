eb65dff30bf964a78ea3065432362f23a5f95085
Hamed Tijane Mkpoumie Monfouayi <hmkpoumiemonfouayi@dept-info.crosemont.quebec>
2021-04-30 15:34:43 -0400

    Implémentation du code pour l'entité et le dao d'une sauvegarde

--- db/create_db.sql
+++ db/create_db.sql
@@ -6,17 +6,16 @@ CREATE TABLE `user` (
 );
 CREATE TABLE `sauvegarde` (
 	`username`			varchar(255) NOT NULL,
-	`question_uri`		varchar(2048) CHARACTER SET latin1 NOT NULL,
+	`question_uri`		varchar(1024) CHARACTER SET latin1 NOT NULL,
 	`date_sauvegarde`	int(10) NOT NULL,
 	`langage`			varchar(255) NOT NULL,
 	`code`				text,
-	PRIMARY KEY (`username`, `question_uri`),
-	FOREIGN KEY (`username`) REFERENCES `user`(`username`),
-	UNIQUE (`langage`)
+	PRIMARY KEY (`username`, `question_uri`, `langage`),
+	FOREIGN KEY (`username`) REFERENCES `user`(`username`)
 );
 CREATE TABLE `avancement` (
 	`username`		varchar(255) NOT NULL,
-	`question_uri` 	varchar(2048) CHARACTER SET latin1 NOT NULL,
+	`question_uri` 	varchar(1024) CHARACTER SET latin1 NOT NULL,
 	`etat`			int DEFAULT 1,
 	`type`          int NOT NULL,
 	PRIMARY KEY (`username`, `question_uri`),
@@ -24,7 +23,7 @@ CREATE TABLE `avancement` (
 );
 CREATE TABLE `reponse_sys` (
 	`username`		varchar(255) NOT NULL,
-	`question_uri` 	varchar(2048) CHARACTER SET latin1,
+	`question_uri` 	varchar(1024) CHARACTER SET latin1,
 	`conteneur`		varchar(64),
 	`reponse`		varchar(255),
 	PRIMARY KEY (`username`, `question_uri`),
@@ -33,7 +32,7 @@ CREATE TABLE `reponse_sys` (
 );
 CREATE TABLE `reponse_prog` (
 	`username`			varchar(255) NOT NULL,
-	`question_uri` 		varchar(2048) CHARACTER SET latin1,
+	`question_uri` 		varchar(1024) CHARACTER SET latin1,
 	`date_soumission` 	int(10) NOT NULL,
 	`langage`			varchar(255) NOT NULL,
 	`code`				text,
