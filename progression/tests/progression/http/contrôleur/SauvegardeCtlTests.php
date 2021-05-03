8550a55b1bfbbb286aafe73939689bc25be3e8a0
Hamed Tijane Mkpoumie Monfouayi <hmkpoumiemonfouayi@dept-info.crosemont.quebec>
2021-05-03 14:54:39 -0400

    Début tests de la sauvegarde automatique

---
+++ progression/tests/progression/http/contrôleur/SauvegardeCtlTests.php
@@ -0,0 +1,90 @@
+<?php
+/*
+   This file is part of Progression.
+
+   Progression is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Progression is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Progression.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+require_once __DIR__ . "/../../../TestCase.php";
+
+use progression\dao\DAOFactory;
+use progression\domaine\entité\{Sauvegarde, User};
+use progression\http\contrôleur\SauvegardeCtl;
+use Illuminate\Http\Request;
+use Illuminate\Auth\GenericUser;
+
+final class SauvegardeCtlTests extends TestCase
+{
+	public $user;
+
+	public function setUp(): void
+	{
+		parent::setUp();
+		$this->user = new GenericUser(["username" => "jdoe", "rôle" => User::ROLE_NORMAL]);
+
+		$_ENV["APP_URL"] = "https://example.com/";
+
+		// UserDAO
+		$mockUserDAO = Mockery::mock("progression\dao\UserDAO");
+		$mockUserDAO
+			->shouldReceive("get_user")
+			->with("jdoe")
+			->andReturn(new User("jdoe"));
+		$mockUserDAO
+			->shouldReceive("get_user")
+			->with("Marcel")
+			->andReturn(null);
+
+        // Question
+		$question = new QuestionProg();
+		$question->type = Question::TYPE_PROG;
+		$question->nom = "appeler_une_fonction_paramétrée";
+		$question->uri = "https://depot.com/roger/questions_prog/fonctions01/appeler_une_fonction";
+
+		$mockQuestionDAO = Mockery::mock("progression\dao\QuestionDAO");
+		$mockQuestionDAO
+			->shouldReceive("get_question")
+			->with("https://depot.com/roger/questions_prog/fonctions01/appeler_une_fonction")
+			->andReturn($question);
+
+		// Sauvegarde
+		$sauvegarde = new Sauvegarde
+        (
+            "jdoe",
+            "aHR0cHM6Ly9kZXBvdC5jb20vcm9nZXIvcXVlc3Rpb25zX3Byb2cvZm9uY3Rpb25zMDEvYXBwZWxlcl91bmVfZm9uY3Rpb24",
+            "03/05/2021",
+            "python",
+            "print(\"Hello world!\")"
+        );
+
+		$mockSauvegardeDAO = Mockery::mock("progression\dao\SauvegardeDAO");
+		$mockSauvegardeDAO
+			->shouldReceive("get_sauvegarde")
+			->with("jdoe", "https://depot.com/roger/questions_prog/fonctions01/appeler_une_fonction", "python")
+			->andReturn($sauvegarde);
+
+		// DAOFactory
+		$mockDAOFactory = Mockery::mock("progression\dao\DAOFactory");
+		$mockDAOFactory->shouldReceive("get_user_dao")->andReturn($mockUserDAO);
+        $mockDAOFactory->shouldReceive("get_question_dao")->andReturn($mockQuestionDAO);
+		$mockDAOFactory->shouldReceive("get_sauvegarde_dao")->andReturn($mockSauvegardeDAO);
+
+		DAOFactory::setInstance($mockDAOFactory);
+	}
+
+	public function tearDown(): void
+	{
+		Mockery::close();
+	}
+}
