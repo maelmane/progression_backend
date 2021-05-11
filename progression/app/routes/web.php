a7236d1e1283fc7349754dcb05d5787cba4960a9
Hamed Tijane Mkpoumie Monfouayi <hmkpoumiemonfouayi@dept-info.crosemont.quebec>
2021-05-11 09:29:10 -0400

    Correction mineure

--- progression/app/routes/web.php
+++ progression/app/routes/web.php
@@ -46,8 +46,8 @@ $router->group(["middleware" => ["auth", "validationPermissions"]], function () 
 	$router->get("/user/{username}/avancements", "NotImplementedCtl@get");
 	// Sauvegarde
 	$router->post("/avancement/{username}/{question_uri}/sauvegardes", "SauvegardeCtl@post");
-	$router->get("/avancement/{username}/{question_uri}/sauvegardes", "NotImplementedCtl@get");
 	$router->get("/sauvegarde/{username}/{question_uri}/{langage}", "SauvegardeCtl@get");
+	$router->get("/avancement/{username}/{question_uri}/sauvegardes", "NotImplementedCtl@get");
 	// Avancement
 	$router->post("/user/{username}/avancements", "AvancementCtl@post");
 	$router->get("/avancement/{username}/{question_uri}", "AvancementCtl@get");
