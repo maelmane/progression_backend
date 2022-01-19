f9b339ab68ffcf54762a9064fcfae6ff43900b93
Patrick Lafrance <patrick.lafrance@gmail.com>
2022-01-18 20:59:39 -0500

    Correction des chemins relatifs

--- progression/tests/exec_tests.sh
+++ progression/tests/exec_tests.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 echo Création de la BD de test sur $DB_SERVERNAME
 
-../db/build_db.sh && \
+DIR=$(dirname "${BASH_SOURCE[0]}")
+$DIR/../db/build_db.sh && \
 mysql --default-character-set=utf8 -h $DB_SERVERNAME -uroot -p$DB_PASSWORD $DB_DBNAME < ./données_de_test.sql || exit 2
 
-../vendor/bin/phpunit --configuration ../phpunit.xml --coverage-text || exit 1
+$DIR/../vendor/bin/phpunit --configuration $DIR/../phpunit.xml --coverage-text || exit 1
 
 echo Suppression de la BD
 echo "DROP DATABASE $DB_DBNAME" | mysql -h $DB_SERVERNAME -uroot -p$DB_PASSWORD || exit 2
 
 echo Analyse statique
-php -d memory_limit=1G ../vendor/bin/phpstan analyse -c ../phpstan.neon || exit 1
+php -d memory_limit=1G $DIR/../vendor/bin/phpstan analyse -c $DIR/../phpstan.neon || exit 1
