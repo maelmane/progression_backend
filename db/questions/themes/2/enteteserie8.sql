571a67a6da83661b253a5f69178fef728ef5acab
Patrick Lafrance <patrick.lafrance@gmail.com>
2022-01-18 20:22:53 -0500

    Supprimé fichiers inutiles

--- db/questions/themes/2/enteteserie8.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO serie (themeID, serieID, numero, titre, description)
-VALUES (2, 8, 1, "Les attributs et les méthodes", "Ces questions vous permettront de vérifier vos connaissances sur les classes, leurs attributs et leurs méthodes.");
