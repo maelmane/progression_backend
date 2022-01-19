2c1c31e8cd1a7219ea3cdfaf134815ea239d40e0
Patrick Lafrance <patrick.lafrance@gmail.com>
2022-01-18 20:49:35 -0500

    Rendu les chemins relatifs

--- progression/tests/exec_tests.sh
+++ progression/tests/exec_tests.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo Création de la BD de test sur $DB_SERVERNAME
 
-$PROGRESSION_DIR/db/build_db.sh && \
-mysql --default-character-set=utf8 -h $DB_SERVERNAME -uroot -p$DB_PASSWORD $DB_DBNAME < $PROGRESSION_DIR/progression/tests/données_de_test.sql || exit 2
+../db/build_db.sh && \
+mysql --default-character-set=utf8 -h $DB_SERVERNAME -uroot -p$DB_PASSWORD $DB_DBNAME < ./données_de_test.sql || exit 2
 
-$PROGRESSION_DIR/progression/vendor/bin/phpunit --configuration $PROGRESSION_DIR/progression/phpunit.xml --coverage-text || exit 1
+../vendor/bin/phpunit --configuration ../phpunit.xml --coverage-text || exit 1
 
 echo Suppression de la BD
 echo "DROP DATABASE $DB_DBNAME" | mysql -h $DB_SERVERNAME -uroot -p$DB_PASSWORD || exit 2
 
 echo Analyse statique
-php -d memory_limit=1G $PROGRESSION_DIR/progression/vendor/bin/phpstan analyse -c $PROGRESSION_DIR/progression/phpstan.neon || exit 1
+php -d memory_limit=1G ../vendor/bin/phpstan analyse -c ../phpstan.neon || exit 1
