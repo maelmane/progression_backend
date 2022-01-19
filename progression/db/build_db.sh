dec4f996faf8f361c17eb0d5b8ff3d32ea12b22b
Patrick Lafrance <patrick.lafrance@gmail.com>
2022-01-18 21:26:29 -0500

    Correction des chemins relatifs, encore

--- progression/db/build_db.sh
+++ progression/db/build_db.sh
@@ -10,4 +10,4 @@ GRANT ALL PRIVILEGES ON $DB_DBNAME.* TO $DB_USERNAME@'%';
 
 EOF
 
-mysql --default-character-set=utf8 -v -h$DB_SERVERNAME -u$DB_USERNAME -p$DB_PASSWORD $DB_DBNAME > /dev/null < $PROGRESSION_DIR/db/create_db.sql
+mysql --default-character-set=utf8 -v -h$DB_SERVERNAME -u$DB_USERNAME -p$DB_PASSWORD $DB_DBNAME > /dev/null < $(dirname ${BASH_SOURCE[0]})/create_db.sql
