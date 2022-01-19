348753cb0b3b4743df2bd6aeba6b4dee7119ec34
Patrick Lafrance <patrick.lafrance@gmail.com>
2022-01-18 21:33:42 -0500

    Correction des chemins encore et encore

--- progression/tests/exec_tests.sh
+++ progression/tests/exec_tests.sh
@@ -3,7 +3,7 @@ echo Création de la BD de test sur $DB_SERVERNAME
 
 DIR=$(dirname "${BASH_SOURCE[0]}")
 $DIR/../db/build_db.sh && \
-mysql --default-character-set=utf8 -h $DB_SERVERNAME -uroot -p$DB_PASSWORD $DB_DBNAME < ./données_de_test.sql || exit 2
+mysql --default-character-set=utf8 -h $DB_SERVERNAME -uroot -p$DB_PASSWORD $DB_DBNAME < $DIR/données_de_test.sql || exit 2
 
 $DIR/../vendor/bin/phpunit --configuration $DIR/../phpunit.xml --coverage-text || exit 1
 
