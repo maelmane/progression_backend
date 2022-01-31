f93aa22268b7a7587bf315b971f0433633222f88
Patrick Lafrance <patrick.lafrance@gmail.com>
2022-01-30 23:22:41 -0500

    Remis TestCase

---
+++ progression/tests/progression/TestCase.php
@@ -0,0 +1,20 @@
+<?php
+namespace progression;
+
+use Laravel\Lumen\Testing\TestCase as BaseTestCase;
+
+/**
+ * Cette classe doit être utilisée pour les tests qui nécessitent l'application Lumen.
+ */
+abstract class TestCase extends BaseTestCase
+{
+	/**
+	 * Creates the application.
+	 *
+	 * @return \Laravel\Lumen\Application
+	 */
+	public function createApplication()
+	{
+		return require __DIR__ . "/../../app/bootstrap/app.php";
+	}
+}
