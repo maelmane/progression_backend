196df4acea909261900ab6908d785eab306cfab9
Patrick Lafrance <plafrance@crosemont.qc.ca>
2022-02-22 20:37:57 -0500

    Implanté le système de migration de BD

--- progression/db/build_db.sh
+++ progression/db/build_db.sh
@@ -1,13 +1,46 @@
-> /dev/null mysql --default-character-set=utf8 -v -h$DB_SERVERNAME -uroot -p$DB_PASSWORD <<EOF 
-DROP DATABASE IF EXISTS $DB_DBNAME;
+# Création initiale
+&>/dev/null mysql --default-character-set=utf8 -v -h$DB_SERVERNAME -uroot -p$DB_PASSWORD <<EOF
+CREATE DATABASE IF NOT EXISTS $DB_DBNAME
+				  CHARACTER SET utf8mb4
+				  COLLATE utf8mb4_general_ci;
+show databases;
+USE $DB_DBNAME;				  
+DROP PROCEDURE IF EXISTS migration;
 
-CREATE USER IF NOT EXISTS $DB_USERNAME@'%' IDENTIFIED BY "$DB_PASSWORD";
-CREATE DATABASE $DB_DBNAME
-	CHARACTER SET utf8mb4
-	COLLATE utf8mb4_general_ci;
+DELIMITER &&
+CREATE PROCEDURE migration()
+    proc: BEGIN
 
-GRANT ALL PRIVILEGES ON $DB_DBNAME.* TO $DB_USERNAME@'%';
+    CREATE TABLE IF NOT EXISTS version (
+    	version int NOT NULL DEFAULT 0
+    );
+
+    SET @version := (SELECT count(version) FROM version);
+    IF @version > 0 THEN
+    	LEAVE proc;
+    END IF;
+    
+    START TRANSACTION;
+    
+    CREATE USER IF NOT EXISTS $DB_USERNAME@'%' IDENTIFIED BY "$DB_PASSWORD";
+    
+    GRANT ALL PRIVILEGES ON $DB_DBNAME.* TO $DB_USERNAME@'%';
+    
+    INSERT INTO version VALUES(0);
+    
+    COMMIT;
+
+END&&
+DELIMITER ;
+
+CALL migration();
+DROP PROCEDURE migration;
 
 EOF
 
-mysql --default-character-set=utf8 -v -h$DB_SERVERNAME -u$DB_USERNAME -p$DB_PASSWORD $DB_DBNAME > /dev/null < $(dirname ${BASH_SOURCE[0]})/create_db.sql
+# Migrations
+for migration in $(ls migrations.d/[0-9]*.sql)
+do
+	echo -n Migration $migration...
+	mysql --default-character-set=utf8 -v -h$DB_SERVERNAME -u$DB_USERNAME -p$DB_PASSWORD $DB_DBNAME > /dev/null < $(dirname ${BASH_SOURCE[0]})/$migration && echo OK
+done
