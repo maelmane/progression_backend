c229d586d9a34dfd31fd31406fc13da5db16a028
Ghiles <1779458@crosemont.qc.ca>
2022-03-02 14:16:36 -0500

    creation de la classe CommentaireDAO

---
+++ progression/app/progression/dao/CommentaireDAO.php
@@ -0,0 +1,105 @@
+<?php
+/*
+   This file is part of Progression.
+
+   Progression is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Progression is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Progression.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace progression\dao;
+
+use mysqli_sql_exception;
+use progression\domaine\entité\Commentaire;
+
+class CommentaireDAO extends EntitéDAO
+{
+	public function get_commentaire($id)
+	{
+		$commentaire = null;
+
+		//$id = null;
+		$message = null;
+		$créateur = null;
+		
+
+		try {
+			$query = EntitéDAO::get_connexion()->prepare(
+				"SELECT message, createur FROM commentaire WHERE id = ?  ",
+			);
+			$query->bind_param("i", $id);
+
+			$query->execute();
+			$query->bind_result($message,$créateur);
+
+			$résultat = $query->fetch();
+			$query->close();
+			if ($résultat) {
+				$commentaire = new Commentaire($id, $message, $créateur);
+			}
+		} catch (mysqli_sql_exception $e) {
+			throw new DAOException($e);
+		}
+
+		return $commentaire;
+	}
+
+	public function get_toutes($créateur)
+	{
+	
+		$commentaires = [];
+
+		$id = null;
+		$message = null;
+	
+
+		try {
+			$query = EntitéDAO::get_connexion()->prepare(
+				"SELECT id, message, FROM commentaire WHERE createur = ? ",
+			);
+			$query->bind_param("c", $créateur);
+
+			$query->execute();
+			$query->bind_result($id, $message);
+
+			while ($query->fetch()) {
+				$commentaires[$id] = new Commentaire(null, $message, $créateur);
+			}
+			$query->close();
+		} catch (mysqli_sql_exception $e) {
+			throw new DAOException($e);
+		}
+
+		return $commentaires;
+	}
+
+	public function save($objet)
+	{
+		try {
+			$query = EntitéDAO::get_connexion()->prepare(
+				"INSERT INTO commentaire ( id, message, createur ) VALUES ( ?, ?, ? )",
+			);
+
+
+			$query->bind_param("imc", $objet->id, $objet->message, $objet->créateur);
+			$query->execute();
+			$query->close();
+		} catch (mysqli_sql_exception $e) {
+			throw new DAOException($e);
+		}
+
+		$commentaire = $this->get_commentaire($objet->id);
+	
+		return $commentaire;
+	}
+
+}
