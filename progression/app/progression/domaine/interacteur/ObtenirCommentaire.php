f42ce09ebe0490a010b95345d1ff568997733d7e
Ghiles <1779458@crosemont.qc.ca>
2022-03-02 14:15:06 -0500

    creation de la classe ObtenirCommentaire

---
+++ progression/app/progression/domaine/interacteur/ObtenirCommentaire.php
@@ -0,0 +1,34 @@
+<?php
+/*
+   This file is part of Progression.
+
+   Progression is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Progression is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Progression.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace progression\domaine\interacteur;
+
+use progression\domaine\entité\Commentaire;
+use progression\dao\DAOFactory;
+
+class ObtenirCommentaire extends Interacteur
+{
+	public function get_commentaire($id)
+	{
+		$dao = DAOFactory::getInstance()->get_commentaire_dao();
+
+		$commentaire = $dao->get_commentaire($id);
+    
+		return $commentaire;
+	}
+}
