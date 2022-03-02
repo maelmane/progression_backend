236be1aed95f80914bcf0ddfc5128a3b7d83617f
Ghiles <1779458@crosemont.qc.ca>
2022-03-02 14:15:29 -0500

    creation de la classe SauvegarderCommentaire

---
+++ progression/app/progression/domaine/interacteur/SauvegarderCommentaire.php
@@ -0,0 +1,42 @@
+<?php
+/*
+   This file is part of Progression.
+
+   Progression is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Progression is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Progression.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace progression\domaine\interacteur;
+
+use progression\domaine\entité\Commentaire;
+use progression\dao\DAOFactory;
+
+class SauvegarderCommentaire extends Interacteur
+{
+	public function sauvegarder_commentaire($id , $message, $créateur)
+	{
+		if (!$message || !$créateur) {
+			return null;
+		}
+
+		$dao = DAOFactory::getInstance()->get_commentaire_dao();
+
+		if ($dao->get_commentaire($id)) {
+			return null;
+		}
+
+		$commentaire = new Commentaire($id , $message, $créateur);
+
+		return $dao->save($commentaire);
+	}
+}
