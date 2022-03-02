ec4b2d43d5ae0db2c2c8adef15383faec84a0dbd
Ghiles <1779458@crosemont.qc.ca>
2022-03-02 14:19:13 -0500

    creation de la classe CommentaireCtl

---
+++ progression/app/progression/http/contrôleur/CommentaireCtl.php
@@ -0,0 +1,42 @@
+<?php
+/*
+   This file is part of Progression.
+
+   Progression is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Progression is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Progression.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace progression\http\contrôleur;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+use progression\domaine\entité\Commentaire;
+use progression\domaine\interacteur\{ObtenirCommentaire, SauvegarderCommentaire};
+
+
+class CommentaireCtl extends Contrôleur
+{
+    public function get(Request $request, $id)
+	{
+		
+		
+	}
+
+	public function post(Request $request, $id)
+	{
+		
+	}
+
+	
+}
