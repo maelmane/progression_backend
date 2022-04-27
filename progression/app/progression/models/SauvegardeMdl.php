4c12c2a5fb3de2fe8a9be3dcd5701fe68a85236f
Patrick Lafrance <plafrance@crosemont.qc.ca>
2022-04-27 11:52:24 -0400

    Ajout du modèle SauvegardeMdl

---
+++ progression/app/progression/models/SauvegardeMdl.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace progression\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SauvegardeMdl extends Model
+{
+	protected $table = "sauvegarde";
+	public $timestamps = false;
+}
+
+?>
