5543bec1021c169a8b88130a99d0f4f1a04f672d
Jade TraverseLHeureux <1619944@crosemont.qc.ca>
2022-05-13 02:02:20 +0000

    Soumettre ses propres jeux de test

--- progression/app/progression/domaine/interacteur/SauvegarderAvancementInt.php
+++ progression/app/progression/domaine/interacteur/SauvegarderAvancementInt.php
@@ -24,11 +24,40 @@ class SauvegarderAvancementInt extends Interacteur
 {
 	public function sauvegarder($username, $question_uri, $nouvelAvancement)
 	{
-		if ($this->source_dao->get_user_dao()->get_user($username) == null) {
-			return null;
-		}
 		$dao_avancement = $this->source_dao->get_avancement_dao();
 		$avancement = $dao_avancement->save($username, $question_uri, $nouvelAvancement);
 		return $avancement;
+	}
+
+	public function récupérer_avancement($username, $question, $tentative)
+	{
+		$dao_avancement = $this->source_dao->get_avancement_dao();
+		$avancement = $dao_avancement->get_avancement($username, $question->uri);
+
+		if ($avancement === null) {
+			$avancement = $this->créer_avancement($question);
+		}
+		$avancement->tentatives[] = $tentative;
+		return $avancement;
+	}
+
+	private function créer_avancement($question)
+	{
+		$avancement = new Avancement(Question::ETAT_NONREUSSI, Question::TYPE_PROG, []);
+		return $avancement;
+	}
+
+	public function mettre_à_jour_dates_et_état($avancement, $date, $username, $question_uri)
+	{
+		if (
+			$avancement->etat != Question::ETAT_REUSSI &&
+			$avancement->tentatives[array_key_last($avancement->tentatives)]->réussi
+		) {
+			$avancement->etat = Question::ETAT_REUSSI;
+			$avancement->date_réussite = $date;
+		}
+
+		$avancement->date_modification = $date;
+		$this->sauvegarder($username, $question_uri, $avancement);
 	}
 }
