19d5b283f52b82fa883f5a4e8f2ce73d18543844
Jade Traverse-L'Heureux <1619944@crosemont.qc.ca>
2022-05-16 09:40:16 -0400

    Ajout de RésultatSysTransformer et de ses tests.

---
+++ progression/app/progression/http/transformer/RésultatSysTransformer.php
@@ -0,0 +1,43 @@
+<?php
+/*
+	This file is part of Progression.
+
+	Progression is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Progression is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Progression.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+namespace progression\http\transformer;
+
+use progression\domaine\entité\RésultatSys;
+use League\Fractal;
+
+class RésultatSysTransformer extends Fractal\TransformerAbstract
+{
+	public $type = "resultat";
+
+	public function transform(RésultatSys $réponse)
+	{
+		$data = [
+			"id" => $réponse->id,
+			"sortie_observée" => $réponse->sortie_observée,
+			"résultat" => $réponse->résultat,
+			"feedback" => $réponse->feedback,
+			"temps_exec" => $réponse->temps_exécution,
+			"links" => (isset($réponse->links) ? $réponse->links : []) + [
+				"self" => "{$_ENV["APP_URL"]}resultat/{$réponse->id}",
+			],
+		];
+
+		return $data;
+	}
+}
