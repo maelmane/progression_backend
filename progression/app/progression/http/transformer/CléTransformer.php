dc43c5e6c1cc7da933cf7b0fea3ec4a0d62a9ec0
Patrick Lafrance <plafrance@crosemont.qc.ca>
2022-05-31 00:25:26 -0400

    Corrections CléTransformer

--- progression/app/progression/http/transformer/CléTransformer.php
+++ progression/app/progression/http/transformer/CléTransformer.php
@@ -27,13 +27,13 @@ class CléTransformer extends BaseTransformer
 	public function transform(Clé $clé)
 	{
 		$data_out = [
-			"id" => $clé->id,
+			"id" => "{$this->id}/{$clé->id}",
 			"secret" => $clé->secret,
 			"création" => $clé->création,
 			"expiration" => $clé->expiration,
 			"portée" => $clé->portée,
 			"links" => (isset($clé->links) ? $clé->links : []) + [
-				"self" => "{$_ENV["APP_URL"]}cle/{$clé->id}",
+				"self" => "{$_ENV["APP_URL"]}cle/{$this->id}/{$clé->id}",
 			],
 		];
 
