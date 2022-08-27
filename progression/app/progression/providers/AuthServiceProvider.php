b87b61512e5902b32b54f4f7d2eb601d2dc8d811
Patrick Lafrance <plafrance@crosemont.qc.ca>
2022-08-27 14:12:56 -0400

    Validation du format de la ressource à autoriser

--- progression/app/progression/providers/AuthServiceProvider.php
+++ progression/app/progression/providers/AuthServiceProvider.php
@@ -130,8 +130,9 @@ class AuthServiceProvider extends ServiceProvider
 		if ($ressources) {
 			foreach ($ressources as $ressource) {
 				if (
-					strlen($ressource["url"]) > 0 &&
-					strlen($ressource["method"]) > 0 &&
+                    is_array($ressource) &&
+					array_key_exists("url", $ressource) && strlen($ressource["url"]) > 0 &&
+                    array_key_exists("method", $ressource) && strlen($ressource["method"]) > 0 &&
 					preg_match("#" . $ressource["url"] . "#", $request->path()) &&
 					preg_match("#" . $ressource["method"] . "#i", $request->method())
 				) {
