4dcef454cb8b667457a3fc10559201065051543e
Patrick Lafrance <plafrance@crosemont.qc.ca>
2023-04-02 19:02:10 +0000

    Améliorer la construction des images pour le CI/CD

--- image-ci/install.el
+++
@@ -1,9 +0,0 @@
-;; Bug fix : https://debbugs.gnu.org/cgi/bugreport.cgi?bug=34341
-(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
-
-
-(require 'package)
-(package-initialize)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-(package-refresh-contents)
-(package-install 'ob-napkin)
