57e61b714b8f82029fef25830c2f7a019341c387
Kessi Mohand <mkessi@ordralphabetix>
2024-02-19 22:28:07 -0500

    Ajout de la classe d'énumération Occupation

---
+++ progression/app/progression/domaine/entité/user/Occupation.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace progression\domaine\entité\user;
+
+enum Occupation:string
+{
+    case ÉTUDIANT = "étudiant";
+    case ENSEIGNANT = "enseignant";
+    case TUTEUR = "tuteur";
+}
+
+?>
