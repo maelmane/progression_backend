5b10e6dcd7824b2fffebedc4ad280d53b3d4184d
Kessi Mohand <mkessi@ordralphabetix>
2024-02-27 14:55:52 -0500

    Adaptation du contrôleur Profil pour utiliser le Transformer

--- progression/app/progression/http/contrôleur/UserProfileCtl.php
+++ progression/app/progression/http/contrôleur/UserProfileCtl.php
@@ -16,58 +16,112 @@
    along with Progression.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+
 namespace progression\http\contrôleur;
 
-//Contrôleur des profiles utilisateur
+use progression\domaine\entité\user\{User, Occupation, État, Rôle};
+use progression\http\transformer\ProfilTransformer;
+
 class UserProfileCtl extends Contrôleur
 {
-    public function getProfile(string $username): String
+    public function getProfile(string $username): string
     {
-        // Profils factices pour les tests Front-End
-        $profils = [
-            "martine" => [
-                "nom" => "Bouchard",
-                "prénom" => "Martine",
-                "nom_complet" => "Martine Bouchard",
-                "pseudo" => "Didine",
-                "biographie" => "Passionnée de la programmation bas-niveau, j'ai trouvé un réel intérêt pour les systèmes d'exploitation",
-                "occupation" => "enseignant",
-                "avatar" => "https://static.vecteezy.com/ti/vecteur-libre/p1/5878308-programmeur-travaillant-moderne-concept-plat-pour-la-conception-de-bannieres-web-femme-developpeur-travaille-sur-ordinateur-portable-et-programmes-en-php-et-autres-langages-de-programmation-illustrationle-avec-scene-de-personnes-isolees-vectoriel.jpg"
-            ],
-            "david" => [
-                "nom" => "Lefevre",
-                "prénom" => "David",
-                "nom_complet" => "David Lefevre",
-                "pseudo" => "DavLev",
-                "biographie" => "Développeur passionné par les nouvelles technologies et les applications web.",
-                "occupation" => "programmeur",
-                "avatar" => "lien_de_l_avatar_de_david.jpg"
-            ],
-            "simon" => [
-                "nom" => "Tremblay",
-                "prénom" => "Simon",
-                "nom_complet" => "Simon Tremblay",
-                "pseudo" => "SimTrem",
-                "biographie" => "Étudiant en informatique avec une passion pour l'intelligence artificielle.",
-                "occupation" => "étudiant",
-                "avatar" => "lien_de_l_avatar_de_simon.jpg"
-            ],
-            "judy" => [
-                "nom" => "Lavoie",
-                "prénom" => "Judy",
-                "nom_complet" => "Judy Lavoie",
-                "pseudo" => "JudLav",
-                "biographie" => "Étudiante dévouée à l'éducation des jeunes générations, spécialisée en sciences informatiques.",
-                "occupation" => "tuteur",
-                "avatar" => "lien_de_l_avatar_de_judy.jpg"
-            ]
-        ];
 
-        // Retour du bon profil, cette fonction changera pour être remplacée par l'appel à l'interacteur..
-        if (isset($profils[$username])) {
-            return json_encode($profils[$username], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
-        } else {
+        $user = $this->getUserByUsername($username);
+
+
+        if ($user === null) {
             return json_encode(["erreur" => "Profil non trouvé"], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+        }
+
+
+        $transformer = new ProfilTransformer();
+        $transformedData = $transformer->transform($user);
+
+
+        return json_encode($transformedData, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+    }
+
+
+    private function getUserByUsername(string $username): ?User
+    {
+
+        switch ($username) {
+            case 'martine':
+                return new User(
+                    'martine',
+                    time(),
+                    'MartineBouchard@gmail.com',
+                    État::ACTIF,
+                    Rôle::ADMIN,
+                    ['avancement1', 'avancement2'],
+                    ['clé1' => 'valeur1', 'clé2' => 'valeur2'],
+                    'Préférences utilisateur 1',
+                    'Bouchard',
+                    'Martine',
+                    'Martine Phillipa Bouchard',
+                    'Didine',
+                    'Enseignante depuis toujours, je suis passionée de programmation orientée objet',
+                    Occupation::ENSEIGNANT,
+                    'https://thumbs.dreamstime.com/z/avatar-de-programmeur-langue-logiciel-110589785.jpg'
+                );
+            case 'david':
+                return new User(
+                    'david',
+                    time(),
+                    'DavidLefevre@gmail.com',
+                    État::ACTIF,
+                    Rôle::NORMAL,
+                    ['avancement3', 'avancement4'],
+                    ['clé3' => 'valeur3', 'clé4' => 'valeur4'],
+                    'Préférences utilisateur 2',
+                    'Lefevre',
+                    'David',
+                    'David Lefevre',
+                    'DavLev',
+                    'Développeur passionné par les nouvelles technologies et les applications web.',
+                    Occupation::PROGRAMMEUR,
+                    'lien_de_l_avatar_de_david.jpg'
+                );
+            case 'simon':
+                return new User(
+                    'simon',
+                    time(),
+                    'SimonTremblay@gmail.com',
+                    État::ACTIF,
+                    Rôle::NORMAL,
+                    ['avancement5', 'avancement6'],
+                    ['clé5' => 'valeur5', 'clé6' => 'valeur6'],
+                    'Préférences utilisateur 3',
+                    'Tremblay',
+                    'Simon',
+                    'Simon Tremblay',
+                    'SimTrem',
+                    'Étudiant en informatique avec une passion pour l\'intelligence artificielle.',
+                    Occupation::ÉTUDIANT,
+                    'lien_de_l_avatar_de_simon.jpg'
+                );
+            case 'judy':
+                return new User(
+                    'judy',
+                    time(),
+                    'JudyLavoie@gmail.com',
+                    État::ACTIF,
+                    Rôle::NORMAL,
+                    ['avancement7', 'avancement8'],
+                    ['clé7' => 'valeur7', 'clé8' => 'valeur8'],
+                    'Préférences utilisateur 4',
+                    'Lavoie',
+                    'Judy',
+                    'Judy Lavoie',
+                    'JudLav',
+                    'Étudiante dévouée à l\'éducation des jeunes générations, spécialisée en sciences informatiques.',
+                    Occupation::TUTEUR,
+                    'lien_de_l_avatar_de_judy.jpg'
+                );
+            // Si quelqu'un veut ajouter d'autres users pour tests
+            default:
+                return null; // Retourne null si le nom d'utilisateur n'est pas trouvé
         }
     }
 }
