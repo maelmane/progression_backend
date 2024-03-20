6e1ada662e8e63d77a00bd939702a0b2ae88f489
Mane Mael <mmane@ordralphabetix>
2024-03-19 22:22:53 -0400

    correction des test existants en ajoutant les champs du profil

--- progression/app/progression/dao/UserDAO.php
+++ progression/app/progression/dao/UserDAO.php
@@ -132,7 +132,7 @@ class UserDAO extends EntitéDAO
 				pseudo: $item["pseudo"] ?? "",
 				biographie: $item["biographie"] ?? "",
 				occupation: Occupation::ETUDIANT,
-				avatar: $item["avatar"] ?? "bonjour",
+				avatar: $item["avatar"] ?? "",
 			);
 		}
 		return $users;
